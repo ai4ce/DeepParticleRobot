4bed114c30622659681af72d8a24b6f5a891f610
jshen04 <66543503+jshen04@users.noreply.github.com>
2021-10-13 23:24:23 -0400

    Add baseline training files

---
+++ script/obstacle-navigation/train_ppo.py
@@ -0,0 +1,116 @@
+import gym
+from gym_dpr.envs.viz import Visualizer
+import gym_dpr.envs.DPR_ParticleRobot as DPR_ParticleRobot
+from gym_dpr.envs.DPR_ParticleRobot import CircularBot
+from gym_dpr.envs.DPR_SuperAgent import SuperCircularBot
+from gym_dpr.envs.DPR_World import World
+
+import math
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.monitor import Monitor, ResultsWriter
+from stable_baselines3.common.results_plotter import plot_results, plot_curves
+
+learning_rate = 0.0001
+n_steps = 10000
+batch_size = 16
+n_epochs = 10
+gamma = 1.0
+gae_lambda = 0.99
+clip_range = 0.2
+clip_range_vf = None
+ent_coef = 0.0
+vf_coef = 0.5
+max_grad_norm = 10
+use_sde = False
+sde_sample_freq = -1
+target_kl = None
+tensorboard_log = "ppo_obstacle_navigation_log"
+create_eval_env = False
+policy_kwargs = dict(net_arch=[64, 128, 128, 64])
+verbose = 1
+seed = None
+device = "auto"
+_init_setup_model = True
+
+
+def linear_schedule(initial_value: float):
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+    def func(progress_remaining: float):
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return progress_remaining * initial_value
+    return func
+
+for idx in range(3):
+  # REGISTER ENV
+  env = gym.make('dpr_single-v0',
+                 numBots=9, worldClass=World, botClass=CircularBot, superBotClass=SuperCircularBot,
+                 discreteActionSpace=True, continuousAction=False,
+                 goalFrame=True,
+                 rewardFunc="piecewise",
+                 randomSeed=idx,
+                 fixedStart=False, fixedGoal=True,
+                 fixedStartCoords=None, fixedGoalCoords=(0, 0),
+                 polarStartCoords=True, polarGoalCoords=False,
+                 transformRectStart=(0, 0), transformRectGoal=(0, 0),
+                 xLower=-1000, xUpper=1000, yLower=-1000, yUpper=1000,
+                 radiusLower=450, radiusUpper=550, angleLower=0, angleUpper=2 * math.pi,
+                 numDead=0, deadIxs=None,
+                 gate=True, gateSize=150,
+                 manipulationTask=False, objectType="Ball", objectPos=None, initializeObjectTangent=True, objectDims=[100, 30],
+                 visualizer=None, recordInfo=False)
+  check_env(env)
+  # INITIALIZE MODEL
+  model = PPO("MlpPolicy",
+              env,
+              learning_rate = linear_schedule(0.005),
+              n_steps = n_steps,
+              batch_size = batch_size,
+              n_epochs = n_epochs,
+              gamma = gamma,
+              gae_lambda = gae_lambda,
+              clip_range = clip_range,
+              clip_range_vf = clip_range_vf,
+              ent_coef = ent_coef,
+              vf_coef = vf_coef,
+              max_grad_norm = max_grad_norm,
+              use_sde = use_sde,
+              sde_sample_freq = sde_sample_freq,
+              target_kl = target_kl,
+              tensorboard_log = tensorboard_log,
+              create_eval_env = create_eval_env,
+              policy_kwargs = policy_kwargs,
+              verbose = verbose,
+              seed = seed,
+              device = device,
+              _init_setup_model = _init_setup_model)
+
+  #TRAIN THE MODEL
+  model.learn(total_timesteps=1000000)
+  model.save("ppo_dpr_obstacle_navigation_single_{}".format(idx))
+
+# SAVE AND LOAD
+# model.save("ppo_dpr_obstacle_navigation_single")
+# del model
+# model = PPO.load("ppo_dpr_obstacle_navigation_single")
+#
+# # TEST THE POLICY AND RENDER
+# obs = env.reset()
+# while True:
+#     action, _states = model.predict(obs)
+#     obs, reward, dones, info = env.step(action)
+#     env.render(2500)
+#     if dones:
+#         break
---
+++ script/unresponsive-particles/train_dqn.py
@@ -0,0 +1,120 @@
+import gym
+from gym_dpr.envs.viz import Visualizer
+import gym_dpr.envs.DPR_ParticleRobot as DPR_ParticleRobot
+from gym_dpr.envs.DPR_ParticleRobot import CircularBot
+from gym_dpr.envs.DPR_SuperAgent import SuperCircularBot
+from gym_dpr.envs.DPR_World import World
+
+import math
+
+from stable_baselines3 import DQN
+from stable_baselines3.common.env_checker import check_env
+# from stable_baselines3.common.monitor import Monitor, ResultsWriter
+from stable_baselines3.common.results_plotter import plot_results, plot_curves
+
+learning_rate=0.001
+buffer_size=240000
+learning_starts=0
+batch_size=16
+tau=1.0
+gamma=1.0
+train_freq=4
+gradient_steps=1
+replay_buffer_class=None
+replay_buffer_kwargs=None
+optimize_memory_usage=False
+target_update_interval=40000
+exploration_fraction=0.1
+exploration_initial_eps=1.0
+exploration_final_eps=0.065
+max_grad_norm=10
+tensorboard_log="dqn_unresponsive_particles_log"
+create_eval_env=False
+policy_kwargs=dict(net_arch=[64, 128, 128, 64])
+verbose=1
+seed=None
+device='auto'
+_init_setup_model=True
+
+
+def linear_schedule(initial_value: float):
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+    def func(progress_remaining: float):
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return progress_remaining * initial_value
+    return func
+
+for idx in range(3):
+  # REGISTER ENV
+  env = gym.make('dpr_single-v0',
+                numBots=9, worldClass=World, botClass=CircularBot, superBotClass=SuperCircularBot,
+                discreteActionSpace=False, continuousAction=False,
+                goalFrame=True,
+                rewardFunc="piecewise",
+                randomSeed=idx,
+                fixedStart=False, fixedGoal=True,
+                fixedStartCoords=None, fixedGoalCoords=(0, 0),
+                polarStartCoords=True, polarGoalCoords=False,
+                xLower=-1000, xUpper=1000, yLower=-1000, yUpper=1000,
+                radiusLower=450, radiusUpper=550, angleLower=0, angleUpper=2 * math.pi,
+                numDead=2, deadIxs=None,
+                gate=False, gateSize=150,
+                manipulationTask=False, objectType="Ball", objectPos=None, initializeObjectTangent=True, objectDims=[100, 30],
+                visualizer=None, recordInfo=False)
+  check_env(env)
+
+  # INITIALIZE MODEL
+  model = DQN("MlpPolicy",
+              env,
+              learning_rate = linear_schedule(0.005),
+              buffer_size=buffer_size,
+              learning_starts=learning_starts,
+              batch_size=batch_size,
+              tau=tau,
+              gamma=gamma,
+              train_freq=train_freq,
+              gradient_steps=gradient_steps,
+              replay_buffer_class=replay_buffer_class,
+              replay_buffer_kwargs=replay_buffer_kwargs,
+              optimize_memory_usage=optimize_memory_usage,
+              target_update_interval=target_update_interval,
+              exploration_fraction=exploration_fraction,
+              exploration_initial_eps=exploration_initial_eps,
+              exploration_final_eps=exploration_final_eps,
+              max_grad_norm=max_grad_norm,
+              tensorboard_log=tensorboard_log,
+              create_eval_env=create_eval_env,
+              policy_kwargs=policy_kwargs,
+              verbose=verbose,
+              seed=seed,
+              device=device,
+              _init_setup_model=_init_setup_model)
+
+  # TRAIN THE MODEL
+  model.learn(total_timesteps=1000000)
+  model.save("deepq_dpr_unresponsive_particles_single_{}".format(idx))
+
+# SAVE AND LOAD
+# model.save("deepq_dpr_unresponsive_particles_single")
+# del model
+# model = DQN.load("deepq_dpr_unresponsive_particles_single")
+#
+# # TEST THE POLICY AND RENDER
+# obs = env.reset()
+# while True:
+#     action, _states = model.predict(obs)
+#     obs, reward, dones, info = env.step(action)
+#     env.render(2500)
+#     if dones:
+#         break
